9282e0fee682e06384b4ed7fc89e339cd258ca19
Micha Alt <mail@lean-stack.de>
2022-05-11 22:22:17 +0200

    Install react with typings and turn app into react app.

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+// Global styles
+import './main.css';
+
+// Create render container
+import {createRoot} from 'react-dom/client'
+const root = createRoot(document.getElementById('root') as HTMLElement)
+
+// Render app
+import React from 'react';
+import App from './App';
+root.render(<App />);
