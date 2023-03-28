6688f6522c026590c153aae4a7e3d8712268eb72
Micha Buchholz <mail@lean-stack.de>
2023-03-28 06:04:28 +0200

    Revert the last merge from tailwind branch.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <header className="flex items-center px-8 sm:px-4 bg-white shadow">
-      <h1 className="py-4 text-2xl font-semibold">Lean React</h1>
-    </header>
-  );
+  return <h1>Lean React</h1>;
 }
 
 export default App;
